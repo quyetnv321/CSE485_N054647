70f73d15376e801edfc1d6118ef8e1e37fc701c1
Quyen <quyenta62@wru.vn>
2018-12-30 19:40:09 +0700

    points for days offline

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -12,16 +12,30 @@ class LoginController extends Controller
             'user_name' => $request->userName,
             'password' => $request->password
         );
-        
         if(Auth::attempt($user_data)) {
-            // update lượt chơi ngày mới
+            date_default_timezone_set('Asia/Ho_Chi_Minh');
+            // update lượt chơi, điểm ngày mới
             $time_login = strtotime(Auth::user()->updated_at);
             $timeMidnight = strtotime('today midnight');
-            if($time_login < $timeMidnight) {
+            $timeNow = time();
+            $aDay = 86400;  // 1 ngày = 86400s
+
+            if($time_login < $timeMidnight) {   // lần đầu đăng nhập trong ngày
                 $IdUser = Auth::user()->id;
+                $ScoreUser = Auth::user()->scores;
+                $QuestionDay = Auth::user()->questions_day;
+                $dayMidnight = date('d', $timeMidnight);
+                $dayLogin = date('d', $time_login);
+                $totalDayOff = ($dayMidnight - $dayLogin) - 1; // số ngày ko online
+                if($QuestionDay > 0 ) { // số câu hỏi chưa hoàn thành của những ngày hôm trc sẽ bị tính full điểm
+                    $QuestionDay = $QuestionDay + $totalDayOff * 15; // số câu hỏi ko hoàn thành
+                    $score = $QuestionDay * 15;
+                    $ScoreUser += $score;
+                    $user::where('id',$IdUser)->update(['scores' => $ScoreUser]);
+                }
                 $user::where('id',$IdUser)->update(['questions_day' => 15]);
             }
-            //end update lượt chơi ngày mới
+            //end update lượt chơi, điểm ngày mới
             return redirect()->route('game');
         }
         else
@@ -39,3 +53,4 @@ class LoginController extends Controller
     }
 }
 ?>
+<!-- đang bị lỗi số giờ nhỏ quá đell chia đc cho 8600s ( toàn = 0) -->
